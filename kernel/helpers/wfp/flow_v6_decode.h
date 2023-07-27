269315d2e4a47f428e33f27f44bb8eb49cd1915c
ps <ps@ps>
2023-07-27 18:30:55 +0300

    flow v6 fixed values parsing

--- kernel/helpers/wfp/flow_v6_decode.h
+++ kernel/helpers/wfp/flow_v6_decode.h
@@ -23,6 +23,48 @@ namespace wfp::decode
     };
 
     template<>
+    struct ValueType<FWPS_FIELDS_ALE_FLOW_ESTABLISHED_V6, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_REMOTE_ADDRESS>
+    {
+        using type = FWP_BYTE_ARRAY16*;
+    };
+
+    template<>
+    struct ValueType<FWPS_FIELDS_ALE_FLOW_ESTABLISHED_V6, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_ADDRESS_TYPE>
+    {
+        using type = UINT8;
+    };
+
+    template<>
+    struct ValueType<FWPS_FIELDS_ALE_FLOW_ESTABLISHED_V6, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_DESTINATION_ADDRESS_TYPE>
+    {
+        using type = UINT8;
+    };
+
+    template<>
+    struct ValueType<FWPS_FIELDS_ALE_FLOW_ESTABLISHED_V6, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_PORT>
+    {
+        using type = UINT16;
+    };
+
+    template<>
+    struct ValueType<FWPS_FIELDS_ALE_FLOW_ESTABLISHED_V6, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_REMOTE_PORT>
+    {
+        using type = UINT16;
+    };
+
+    template<>
+    struct ValueType<FWPS_FIELDS_ALE_FLOW_ESTABLISHED_V6, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_INTERFACE>
+    {
+        using type = UINT64*;
+    };
+
+    template<>
+    struct ValueType<FWPS_FIELDS_ALE_FLOW_ESTABLISHED_V6, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_DIRECTION>
+    {
+        using type = UINT32;
+    };
+
+    template<>
     consteval UINT16 LayerId<FWPS_FIELDS_ALE_FLOW_ESTABLISHED_V6>() { return FWPS_LAYER_ALE_FLOW_ESTABLISHED_V6; }
 
     consteval FWP_DATA_TYPE TypeForField(FWPS_FIELDS_ALE_FLOW_ESTABLISHED_V6 field)
@@ -34,7 +76,18 @@ namespace wfp::decode
         case FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_ALE_USER_ID:
             return FWP_TOKEN_ACCESS_INFORMATION_TYPE;
         case FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_ADDRESS:
+        case FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_REMOTE_ADDRESS:
             return FWP_BYTE_ARRAY16_TYPE;
+        case FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_ADDRESS_TYPE:
+        case FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_DESTINATION_ADDRESS_TYPE:
+            return FWP_UINT8;
+        case FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_PORT:
+        case FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_REMOTE_PORT:
+            return FWP_UINT16;
+        case FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_DIRECTION:
+            return FWP_UINT32;
+        case FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_INTERFACE:
+            return FWP_UINT64;
         }
     }
 }
--- kernel/net_data_mon/flow_v6.cpp
+++ kernel/net_data_mon/flow_v6.cpp
@@ -2,46 +2,98 @@
 
 #include <wfp/flow_v6_decode.h>
 
-namespace flow
+void flow::v6::classify(const FWPS_INCOMING_VALUES0* inFixedValues,
+    [[maybe_unused]] const FWPS_INCOMING_METADATA_VALUES0* inMetaValues,
+    [[maybe_unused]] void* layerData,
+    [[maybe_unused]] const void* classifyContext,
+    [[maybe_unused]] const FWPS_FILTER3* filter,
+    [[maybe_unused]] UINT64 flowContext,
+    [[maybe_unused]] FWPS_CLASSIFY_OUT0* classifyOut)
 {
-    namespace v6
+    using namespace wfp::util;
+
+    NTSTATUS status{};
+    const auto* appPathBlob = GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_ALE_APP_ID, inFixedValues);
+    if (!NT_SUCCESS(status))
     {
-        void classify([[maybe_unused]] const FWPS_INCOMING_VALUES0* inFixedValues,
-            [[maybe_unused]] const FWPS_INCOMING_METADATA_VALUES0* inMetaValues,
-            [[maybe_unused]] void* layerData,
-            [[maybe_unused]] const void* classifyContext,
-            [[maybe_unused]] const FWPS_FILTER3* filter,
-            [[maybe_unused]] UINT64 flowContext,
-            [[maybe_unused]] FWPS_CLASSIFY_OUT0* classifyOut)
-        {
-            NTSTATUS status{};
-            const auto* appPathBlob = GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_ALE_APP_ID, inFixedValues);
-            if (!NT_SUCCESS(status))
-            {
-                TraceError("failed to get app path", TraceLoggingNTStatus(status));
-                return;
-            }
-            TraceInfo("app path", TraceLoggingWideString(reinterpret_cast<const wchar_t*>(appPathBlob->data), "app path"));
-
-            const auto* userBlob = GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_ALE_USER_ID, inFixedValues);
-            const auto* accessInformation = reinterpret_cast<const TOKEN_ACCESS_INFORMATION*>(userBlob->data);
-            TraceInfo("user", TraceLoggingSid(static_cast<SID*>(accessInformation->SidHash->SidAttr->Sid), "user"));
-
-            const auto* localAddress = GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_ADDRESS, inFixedValues);
-            if (!NT_SUCCESS(status))
-            {
-                TraceError("failed to get local address", TraceLoggingNTStatus(status));
-                return;
-            }
-            TraceInfo("local address", TraceLoggingIPv6Address(localAddress->byteArray16, "local address"));
-        }
-
-        NTSTATUS notify([[maybe_unused]] FWPS_CALLOUT_NOTIFY_TYPE notifyType,
-            [[maybe_unused]] const GUID* filterKey,
-            [[maybe_unused]] FWPS_FILTER3* filter)
-        {
-            return STATUS_SUCCESS;
-        }
+        TraceError("failed to get app path", TraceLoggingNTStatus(status));
+        return;
     }
+    TraceInfo("app path", TraceLoggingWideString(reinterpret_cast<const wchar_t*>(appPathBlob->data), "app path"));
 
+    const auto* userBlob = GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_ALE_USER_ID, inFixedValues);
+    const auto* accessInformation = reinterpret_cast<const TOKEN_ACCESS_INFORMATION*>(userBlob->data);
+    TraceInfo("user", TraceLoggingSid(static_cast<SID*>(accessInformation->SidHash->SidAttr->Sid), "user"));
+
+    const auto* localAddress = GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_ADDRESS, inFixedValues);
+    if (!NT_SUCCESS(status))
+    {
+        TraceError("failed to get local address", TraceLoggingNTStatus(status));
+        return;
+    }
+    TraceInfo("local address", TraceLoggingIPv6Address(localAddress->byteArray16, "local address"));
+
+    const auto localAddressType = NL_ADDRESS_TYPE(GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_ADDRESS_TYPE, inFixedValues));
+    if (!NT_SUCCESS(status))
+    {
+        TraceError("failed to get llocal address type", TraceLoggingNTStatus(status));
+        return;
+    }
+    TraceInfo("local address type", TraceLoggingUnicodeString(&AddressTypeToString(localAddressType), "local address type"));
+
+    const auto localPort = RtlUshortByteSwap(GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_PORT, inFixedValues));
+    if (!NT_SUCCESS(status))
+    {
+        TraceError("failed to get local port", TraceLoggingNTStatus(status));
+        return;
+    }
+    TraceInfo("local port", TraceLoggingPort(localPort, "local port"));
+
+    const auto* remoteAddress = GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_REMOTE_ADDRESS, inFixedValues);
+    if (!NT_SUCCESS(status))
+    {
+        TraceError("failed to get remote address", TraceLoggingNTStatus(status));
+        return;
+    }
+    TraceInfo("remote address", TraceLoggingIPv6Address(remoteAddress->byteArray16, "remote address"));
+
+    const auto remotePort = RtlUshortByteSwap(GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_REMOTE_PORT, inFixedValues));
+    if (!NT_SUCCESS(status))
+    {
+        TraceError("failed to get remote port", TraceLoggingNTStatus(status));
+        return;
+    }
+    TraceInfo("remote port", TraceLoggingPort(remotePort, "remote port"));
+
+    const auto remoteAddressType = NL_ADDRESS_TYPE(GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_DESTINATION_ADDRESS_TYPE, inFixedValues));
+    if (!NT_SUCCESS(status))
+    {
+        TraceError("failed to get destination address type", TraceLoggingNTStatus(status));
+        return;
+    }
+    TraceInfo("destination address type", TraceLoggingUnicodeString(&AddressTypeToString(remoteAddressType), "remote address type"));
+
+    const auto* localInterfaceLuid = GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_IP_LOCAL_INTERFACE, inFixedValues);
+    if (!NT_SUCCESS(status))
+    {
+        TraceError("failed to get local interface LUID", TraceLoggingNTStatus(status));
+        return;
+    }
+    NT_ASSERT(localInterfaceLuid);
+    TraceInfo("local interface LUID", TraceLoggingValue(*localInterfaceLuid, "local interface LUID"));
+
+    const auto direction = FWP_DIRECTION(GET_VALUE(status, FWPS_FIELD_ALE_FLOW_ESTABLISHED_V6_DIRECTION, inFixedValues));
+    if (!NT_SUCCESS(status))
+    {
+        TraceError("failed to get direction", TraceLoggingNTStatus(status));
+        return;
+    }
+    TraceInfo("direction", TraceLoggingUnicodeString(&DirectionToString(direction), "direction"));
+}
+
+NTSTATUS flow::v6::notify([[maybe_unused]] FWPS_CALLOUT_NOTIFY_TYPE notifyType,
+    [[maybe_unused]] const GUID* filterKey,
+    [[maybe_unused]] FWPS_FILTER3* filter)
+{
+    return STATUS_SUCCESS;
 }
